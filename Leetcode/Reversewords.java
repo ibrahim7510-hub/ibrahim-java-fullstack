a76febe42b67c6a7d1ef87c88a23da479a2bd17c
ibrahim7510-hub <kmohammedibrahim0507@gmail.com>
2025-12-18 09:22:31 +0530

    Add files via upload

---
+++ Leetcode/Reversewords.java
@@ -0,0 +1,8 @@
+public class Reversewords {
+    public String reverseWords(String s) {
+         String[] words = s.trim().split("\\s+");
+          java.util.Collections.reverse(java.util.Arrays.asList(words)); 
+          return String.join(" ", words);
+        }
+    }
+}
